5a4d9706d3c7840512154f0d183e3d1979d8bdd4
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-06 13:28:53 -0800

    Create REMOVE_FEATURES action / function.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,18 @@
 export const ADD_FEATURES = "ADD_FEATURES";
+export const REMOVE_FEATURES = "REMOVE_FEATURES"
 
 export const addFeatures = feature => {
-    console.log(feature);
+    console.log('addFeature :', feature);
     return {
         type: ADD_FEATURES,
         payload: feature
     };
 };
+
+export const removeFeatures = feature => {
+    console.log('removeFeature:', feature)
+    return {
+        type: REMOVE_FEATURES,
+        payload: feature
+    }
+}
